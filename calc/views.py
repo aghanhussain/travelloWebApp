5b0ab49e02bb44cb804042f6ecc69d03dd56c29b
nasir <aghanhussain@gmail.com>
2019-12-10 14:53:47 +0500

    simpleCalc to add two input number taken via html form

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name':'Nasir'})
+
+def add(request):
+    val1 =int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1+val2
+
+    return render(request, 'result.html', {'result': res})
