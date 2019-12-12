2fac03ca71d98375404a049043c959d822064fdc
nasir <aghanhussain@gmail.com>
2019-12-12 16:13:12 +0500

    passing dynaic data via model class object

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Destination:
+    price : int
+    desc : str
+    name : str
+    id : int
+    image = str
--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = "Karachi"
+    dest1.price = 1000
+    dest1.desc = "The city that never sleeps"
+
+    return render(request, 'index.html',{'dest':dest1})
