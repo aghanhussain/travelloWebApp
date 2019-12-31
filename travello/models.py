3b508dbd5b28d283ab5239bd4b9efb6d06db5f02
nasirhussain <aghanhussain@gmail.com>
2019-12-31 14:16:42 +0500

    make migrations of postgreeSql into PGadmin

--- travello/models.py
+++ travello/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    price : int
-    desc : str
-    name : str
-    id : int
-    image = str
-    offer  = bool
+class Destination(models.Model):
+    price = models.IntegerField()
+    desc = models.TextField()
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='pics')
+    offer  = models.BooleanField(default=False)
+ 
